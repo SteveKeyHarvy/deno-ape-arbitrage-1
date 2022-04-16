5da8be9e108418298c1fcba6e5bc8f6bbe7a6ef1
SteveKeyHarvey <stevekeyharvey@gmail.com>
2022-04-16 18:25:42 -0400

    api_fix_2

--- app.ts
+++ app.ts
@@ -2,6 +2,14 @@
 import { Application, Context } from "https://deno.land/x/abc@v1.3.3/mod.ts";
 // @ts-ignore
 import { test_order, create_order } from './controllers/controller.ts'
+// @ts-ignore
+import { serve } from "https://deno.land/std/http/server.ts";
+
+serve((req) =>
+  new Response("Hello World!", {
+    headers: { "content-type": "text/plain" },
+  })
+);
 
 const app = new Application();
 
@@ -19,6 +27,6 @@ app
     .post('/create_order',create_order)
 
 // listen to port
-app.start();
+app.start({ port: 8080 });
 
 console.log('server is running!')
