c8b043826838621ade4e018c1e8a94e7d58c2f7b
SteveKeyHarvey <stevekeyharvey@gmail.com>
2022-04-16 15:22:33 -0400

    bug fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 
-function handler(req: Request): Response {
+function handler(req: Request), Response {
   return new Response("Hello world");
 }
 
