c6aff0afb7978e2a67be19df4e48b8105f5d4115
SteveKeyHarvey <stevekeyharvey@gmail.com>
2022-04-16 15:37:07 -0400

    2nd try hw

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-addEventListener("fetch", (event) => {
-  const response = new Response("Hello WOrld!", {
-    headers: {"content-type": "application/plain"},
-  });
-  event.respondWith(response);
+import { Application } from "https://deno.land/x/oak@v3.0.0/mod.ts";
+
+const app = new Application();
+
+app.use((ctx) => {
+  ctx.response.body = "Hello World 2nd attempt!";
 });
+
+await app.listen({ port: 8000 });
